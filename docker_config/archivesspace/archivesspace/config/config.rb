e5edb7e2e6e62b183ba0e75458010123ff056dc7
Mohamed Abdul Rasheed <mohideen@umd.edu>
2021-10-26 18:21:51 -0400

    LIBASPACE-318. Upgrade to archivesspace 3.1.0
    - Updated the version number on the Dockerfile
    - Remove aspace-import-excel plugin - It is now part of archivespace core.
    - Added/removed config options that changed in the newer releases.
    
    https://issues.umd.edu/browse/LIBASPACE-318

--- docker_config/archivesspace/archivesspace/config/config.rb
+++ docker_config/archivesspace/archivesspace/config/config.rb
@@ -78,7 +78,7 @@ AppConfig[:solr_url] = ENV['ASPACE_SOLR_URL']
 #AppConfig[:solr_backup_schedule] = "0 0 * * *"
 #AppConfig[:solr_backup_number_to_keep] = 1
 #AppConfig[:solr_backup_directory] = proc { File.join(AppConfig[:data_directory], "solr_backups") }
-## add default solr params, i.e. use AND for search: AppConfig[:solr_params] = { "q.op" => "AND" }
+## add default solr params, i.e. use AND for search: AppConfig[:solr_params] = { "mm" => "AND" }
 ## Another example below sets the boost query value (bq) to boost the relevancy for the query string in the title,
 ## sets the phrase fields parameter (pf) to boost the relevancy for the title when the query terms are in close proximity to
 ## each other, and sets the phrase slop (ps) parameter for the pf parameter to indicate how close the proximity should be
@@ -524,6 +524,7 @@ AppConfig[:record_inheritance] = {
 #
 #AppConfig[:pui_search_results_page_size] = 10
 #AppConfig[:pui_branding_img] = 'archivesspace.small.png'
+#AppConfig[:pui_branding_img_alt_text] = 'ArchivesSpace - a community served by Lyrasis.'
 #AppConfig[:pui_block_referrer] = true # patron privacy; blocks full 'referrer' when going outside the domain
 #
 ## The number of PDFs that can be generated (in the background) at the same time.
@@ -565,9 +566,17 @@ AppConfig[:pui_hide][:search_tab] = true
 #
 ## Enable / disable PUI resource/archival object page actions
 #AppConfig[:pui_page_actions_cite] = true
-#AppConfig[:pui_page_actions_bookmark] = true
 AppConfig[:pui_page_actions_request] = false
 #AppConfig[:pui_page_actions_print] = true
+#
+## Set default/active tab for PUI citation modal. If set to 'true' item citation
+## tab will be active by default; if 'false' item description tab will be active.
+## AppConfig[:pui_page_actions_cite] must be set to true for this to take effect.
+#AppConfig[:pui_active_citation_tab_item] = true
+#
+## Enable / disable search-in-collection form in sidebar when viewing records
+# AppConfig[:pui_search_collection_from_archival_objects] = false
+# AppConfig[:pui_search_collection_from_collection_organization] = false
 #
 ## when a user is authenticated, add a link back to the staff interface from the specified record
 #AppConfig[:pui_enable_staff_link] = true
@@ -702,6 +711,13 @@ AppConfig[:pui_page_actions_request] = false
 ## URL prefix to use in ARK URLs.
 ## In most cases this will be the same as the PUI URL.
 #AppConfig[:ark_url_prefix] = proc { AppConfig[:public_proxy_url] }
+#
+## For Bulk Import:
+## specifies whether the "Load Digital Objects" button is available at the Resource Level
+#AppConfig[:hide_do_load] = false
+#
+## For Agents Export
+#AppConfig[:export_eac_agency_code] = false
 #
 ## Specifies if the fields that show up in csv should be limited to those in search results
 #AppConfig[:limit_csv_fields] = true
